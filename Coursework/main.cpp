63fc6c76f6c823c892991dbc8984b4977da9d437
Jack <pearlsoftheintern@gmail.com>
2024-06-04 12:03:43 +0300

    Added function readFromFile and writeToFile. Added tests to them

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -12,6 +12,23 @@ public:
 
     Person(std::string n, std::string s, int a, std::string st) : name(n), surname(s), age(a), status(st) {}
 };
+void writeToFile(const std::vector<Person>& people, const std::string& filename) {
+    std::ofstream file(filename, std::ios::app);
+    for (const auto& person : people) {
+        file << person.name << " " << person.surname << " " << person.age << " " << person.status << "\n";
+    }
+}
+
+std::vector<Person> readFromFile(const std::string& filename) {
+    std::vector<Person> people;
+    std::ifstream file(filename);
+    std::string name, surname, status;
+    int age;
+    while (file >> name >> surname >> age >> status) {
+        people.push_back(Person(name, surname, age, status));
+    }
+    return people;
+}
 
 int main() {
 
--- TestCourseWork/TestCourseWork.cpp
+++ TestCourseWork/TestCourseWork.cpp
@@ -21,5 +21,30 @@ namespace TestCourseWork
             Assert::AreEqual(std::string("normal"), person.status);
         }
 
+        TEST_METHOD(WriteJohnDoe30normal)
+        {
+            std::vector<Person> people = { Person("John", "Doe", 30, "normal") };
+            writeToFile(people, "/Git/GitProjectAll/GitCoursework/CourseWork/TestCourseWork/TestCourseWork.txt");
+
+            std::ifstream file("/Git/GitProjectAll/GitCoursework/CourseWork/TestCourseWork/TestCourseWork.txt");
+            std::string name, surname, status;
+            int age;
+            file >> name >> surname >> age >> status;
+
+            Assert::AreEqual(std::string("John"), name);
+            Assert::AreEqual(std::string("Doe"), surname);
+            Assert::AreEqual(30, age);
+            Assert::AreEqual(std::string("normal"), status);
+        }
+
+        TEST_METHOD(ReadJohnDoe30normal)
+        {
+            std::vector<Person> people = readFromFile("/Git/GitProjectAll/GitCoursework/CourseWork/TestCourseWork/TestCourseWork.txt");
+            Assert::IsFalse(people.empty());
+            Assert::AreEqual(std::string("John"), people[0].name);
+            Assert::AreEqual(std::string("Doe"), people[0].surname);
+            Assert::AreEqual(30, people[0].age);
+            Assert::AreEqual(std::string("normal"), people[0].status);
+        }
     };
 }
